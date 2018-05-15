b4c76209a5b5ce6d9897f169dab23af505add308
Ayar Portugal <ayar.portugal@amuyaa.com>
2018-05-15 22:20:15 +0200

    Updates

--- amuyana2dev/src/com/amuyana/app/formulas/Formula.java
+++ amuyana2dev/src/com/amuyana/app/formulas/Formula.java
@@ -3,84 +3,82 @@ package com.amuyana.app.formulas;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 public abstract class Formula {
     
     public static HBox implication(int orientation, String element, String antielement){
         HBox implication = new HBox();
         
-        Label op= new Label("(");
+        Label p0= formatedLabelParenthesis("(");
+        Label p1= formatedLabelParenthesis(")");
+        Label p2= formatedLabelParenthesis("(");
+        Label p3= formatedLabelParenthesis(")");
+        
         Label e = null;
         Label se = null;
         Label i=new Label("\u2283");
         Label ae = null;
         Label sae = null;
-        Label cp=new Label(")");
         
         switch(orientation){
             case(0):{
-                e=new Label(element);
-                se=new Label("A");
-                ae=new Label(antielement);
-                sae=new Label("P");
+                e=formatedLabelElement(element);
+                se=formatedLabelSign("A");
+                ae=formatedLabelElement(antielement);
+                sae=formatedLabelSign("P");
                 break;
             }
             case(1):{
-                e=new Label(antielement);
-                se=new Label("A");
-                ae=new Label(element);
-                sae=new Label("P");
+                e=formatedLabelElement(antielement);
+                se=formatedLabelSign("A");
+                ae=formatedLabelElement(element);
+                sae=formatedLabelSign("P");
                 break;
             }
             case(2):{
-                e=new Label(element);
-                se=new Label("T");
-                ae=new Label(antielement);
-                sae=new Label("T");
+                e=formatedLabelElement(element);
+                se=formatedLabelSign("T");
+                ae=formatedLabelElement(antielement);
+                sae=formatedLabelSign("T");
                 break;
             }
         }
         
-        implication.getChildren().addAll(op,e,se,i,ae,sae,cp);
+        
+        i.setFont(Font.font("Arial", FontPosture.REGULAR, 17));
+        
+        
+        implication.getChildren().addAll(p0,e,se,p1,i,p2,ae,sae,p3);
         return implication;
     }
     
+    public static Label formatedLabelParenthesis(String string){
+        Label formatedLabel = new Label(string);
+        
+        formatedLabel.setFont(Font.font("Arial", FontWeight.EXTRA_LIGHT, FontPosture.REGULAR, 17));
+        
+        return formatedLabel;
+    }
+    
+    public static Label formatedLabelElement(String string){
+        Label formatedLabel = new Label(string);
+        
+        formatedLabel.setFont(Font.font("Arial", FontWeight.MEDIUM, FontPosture.REGULAR, 15));
+        
+        return formatedLabel;
+    }
+    public static Label formatedLabelSign(String string){
+        Label formatedLabel = new Label(string);
+        formatedLabel.setFont(Font.font("Arial", FontWeight.LIGHT, FontPosture.REGULAR, 15));
+        //formatedLabel.setTranslateX(5);
+        formatedLabel.setTranslateY(5);
+        return formatedLabel;
+    }
+    
     public static HBox conjunction(int orientation, String element, String antielement){
-        HBox implication = new HBox();
-        
-        Label op= new Label("(");
-        Label e = null;
-        Label se = null;
-        Label i=new Label("\u2283");
-        Label ae = null;
-        Label sae = null;
-        Label cp=new Label(")");
-        
-        switch(orientation){
-            case(0):{
-                e=new Label(element);
-                se=new Label("A");
-                ae=new Label(antielement);
-                sae=new Label("P");
-                break;
-            }
-            case(1):{
-                e=new Label(antielement);
-                se=new Label("A");
-                ae=new Label(element);
-                sae=new Label("P");
-                break;
-            }
-            case(2):{
-                e=new Label(element);
-                se=new Label("T");
-                ae=new Label(antielement);
-                sae=new Label("T");
-                break;
-            }
-        }
-        
-        implication.getChildren().addAll(op,e,se,i,ae,sae,cp);
-        return implication;
+        return null;
     }
 }
--- amuyana2dev/src/com/amuyana/app/gui/tod/TodController.java
+++ amuyana2dev/src/com/amuyana/app/gui/tod/TodController.java
@@ -1,11 +1,14 @@
 
 package com.amuyana.app.gui.tod;
 
+import com.amuyana.app.data.Element;
 import com.amuyana.app.data.Fcc;
 import com.amuyana.app.formulas.Formula;
 import com.amuyana.app.gui.AppController;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -19,7 +22,6 @@ public class TodController implements Initializable {
 
     @FXML private AppController appController;
     @FXML private StackPane pane;
-    @FXML private VBox vbox;
     @FXML private BorderPane borderpane;
     @FXML private ComboBox cobxFcc;
     
@@ -28,7 +30,7 @@ public class TodController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+        manageEvents();
     }
     
     @FXML
@@ -48,14 +50,40 @@ public class TodController implements Initializable {
 
     public void fillData() {
         cobxFcc.setItems(appController.getListFcc());
+        
+    }
+    
+    public void manageEvents(){
+        cobxFcc.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                if(newValue!=null){
+                    draw((Fcc)newValue);
+                }
+            }
+        });
     }
 
+    public void draw(Fcc fcc){
+        pane.getChildren().add(fccContainer(fcc));
+    }
+    
     private TitledPane fccContainer(Fcc fcc){
+        Element e = appController.elementOf(0, fcc);
+        Element ae = appController.elementOf(1, fcc);
         
         VBox content = new VBox(); 
         TitledPane fccContainer = new TitledPane(fcc.toString(), content);
         
-        content.getChildren().add(Formula.implication(0, appController.elementOf(0, fcc), appController.elementOf(1, fcc)));
+        fccContainer.setCollapsible(false);
+        fccContainer.setText(fcc.toString());
+        fccContainer.setMaxWidth(200);
+        
+        content.getChildren().add(Formula.implication(0, e.getSymbol(), ae.getSymbol()));
+        content.getChildren().add(Formula.implication(1, e.getSymbol(), ae.getSymbol()));
+        content.getChildren().add(Formula.implication(2, e.getSymbol(), ae.getSymbol()));
+        
+        
         
         return fccContainer;
     }
